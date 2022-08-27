16624183daa7f976ec0c64822e8b4ae592892370
EluneyMo <morales.eluney.epet20@gmail.com>
2022-08-27 20:08:41 -0300

    Definiendo como constante la imagen 1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ addEventListener ('DOMContentLoaded', () =>){
  const imagenes = ['imagenes/cuidados-principales-de-los-claveles-1280x720x80xX.jpg' , 'imagenes/Imagen 3 girasol.jpg', 'imagenes/R (1).jpg' ]
  
  let i = 1
- const = document.querySelector ('#img1')
+ const img1= document.querySelector ('#img1')
  const = document.querySelector ('#img2')
  const progressbar= document.querySelector ('#progressbar')
 }
