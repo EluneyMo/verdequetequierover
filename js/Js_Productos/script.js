3b661e7b14c58c903b6096a9ffe76a3ba9f0c98b
Luca <lukitasantarelli@gmail.com>
2022-11-05 15:42:04 -0300

    Modifico productos y classes

--- js/Js_Productos/script.js
+++ js/Js_Productos/script.js
@@ -3,6 +3,7 @@ const contDeProductos = document.getElementById('contProductos');
 Plantas.forEach((content) =>{
     const div = document.createElement('div');
     div.classList.add('content');
-    div.innerHTML = '<h3>${content.nombre}</h3>';
+    div.innerHTML = `<h1>${content.nombre}</h1>
+    <p>${content.precio}</p>`;
     contDeProductos.appendChild(div);
 })
