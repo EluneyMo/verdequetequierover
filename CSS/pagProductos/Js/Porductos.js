7a8ad49dae701c2978d00293d60e2f4ea080444f
Luca <lukitasantarelli@gmail.com>
2022-11-05 15:03:21 -0300

    Agrego variable con productos

---
+++ CSS/pagProductos/Js/Porductos.js
@@ -0,0 +1,8 @@
+let Plantas = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",}
+]
