385e0deccae010b1e65b4b73ef5af3a2a8f73e5e
Luca <lukitasantarelli@gmail.com>
2022-11-05 15:49:24 -0300

    Modifico productos y precios

--- js/Js_Productos/Porductos.js
+++ js/Js_Productos/Porductos.js
@@ -1,8 +1,8 @@
 let Plantas = [
-    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
-    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
-    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
-    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
-    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",},
-    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "precio",}
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: 200,},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: 300,},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: 300,},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: 300,},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: 1000,},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: 1000,}
 ]
