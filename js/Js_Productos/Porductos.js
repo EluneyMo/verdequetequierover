c9b03e8b69e3f5d5fca61bef9549ecf2e1f6f657
Luca <lukitasantarelli@gmail.com>
2022-11-06 00:43:17 -0300

    Agrego mas contenido

--- js/Js_Productos/Porductos.js
+++ js/Js_Productos/Porductos.js
@@ -1,4 +1,4 @@
-let Plantas = [
+let Suculentas = [
     {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
     {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
     {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
@@ -8,4 +8,15 @@ let Plantas = [
     {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
     {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
     {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+let Acuaticas = [
+    {id: 1, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
 ]
--- js/Js_Productos/script.js
+++ js/Js_Productos/script.js
@@ -1,9 +1,19 @@
-const contDeProductos = document.getElementById('suculentas');
+const contProSuculentas = document.getElementById('suculentas');
 
-Plantas.forEach((content) =>{
+Suculentas.forEach((content) =>{
     const div = document.createElement('div');
     div.classList.add('content');
     div.innerHTML = `<h1>${content.nombre}</h1>
     <p>${content.precio}</p>`;
-    contDeProductos.appendChild(div);
+    contProSuculentas.appendChild(div);
+})
+
+const contProAcuaticas = document.getElementById('Acuaticas');
+
+Acuaticas.forEach((content) =>{
+    const div = document.createElement('div');
+    div.classList.add('content');
+    div.innerHTML = `<h1>${content.nombre}</h1>
+    <p>${content.precio}</p>`;
+    contProAcuaticas.appendChild(div);
 })
