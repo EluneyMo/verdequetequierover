38f211ad256a0ab4c55e22469f64bf3e541b07d6
Luca <lukitasantarelli@gmail.com>
2022-11-07 19:39:51 -0300

    Agrego cambios de otra rama

--- js/Js_Productos/Porductos.js
+++ js/Js_Productos/Porductos.js
@@ -9,14 +9,111 @@ let Suculentas = [
     {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
     {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
 ]
+
 let Acuaticas = [
-    {id: 1, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$200"},
-    {id: 2, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$300"},
-    {id: 3, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$300"},
-    {id: 4, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$300"},
-    {id: 5, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
-    {id: 6, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
-    {id: 7, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
-    {id: 8, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
-    {id: 9, nombre: "Acuaticas", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Arbustos = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Arboles = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Bulbosas = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Cactus = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Plantas_Carnivoras = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Enredaderas = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Folres = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Herbáceas = [
+    {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
+    {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 4, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
+    {id: 5, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 6, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$1.000"},
+    {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
+    {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
+    {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
 ]
