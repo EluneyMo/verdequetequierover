7f59839cb5fa105339b5e5b29d45547c234e3daf
EluneyMo <morales.eluney.epet20@gmail.com>
2022-11-07 10:10:40 -0300

    Agrego apartados de productos en js

--- js/Js_Productos/Porductos.js
+++ js/Js_Productos/Porductos.js
@@ -1,4 +1,4 @@
-let Plantas = [
+let Suculentas = [
     {id: 1, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$200"},
     {id: 2, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
     {id: 3, nombre: "Suculentas", cantidad: 1, desc: "Sin definir", precio: "$300"},
@@ -8,4 +8,40 @@ let Plantas = [
     {id: 7, nombre: "Cactus", cantidad: 1, desc: "Sin definir", precio: "$2.000"},
     {id: 8, nombre: "Planta", cantidad: 1, desc: "Sin definir", precio: "$10.000"},
     {id: 9, nombre: "Algo", cantidad: 1, desc: "Sin definir", precio: "$100.000"}
+]
+
+let Acuaticas = [
+
+]
+
+let Arbustos = [
+
+]
+
+let Arboles = [
+
+]
+
+let Bulbosas = [
+
+]
+
+let Cactus = [
+
+]
+
+let Plantas Carnivoras = [
+
+]
+
+let Enredaderas = [
+
+]
+
+let Folres = [
+
+]
+
+let Herbáceas = [
+
 ]
