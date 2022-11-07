ea0f78a2f53baeb40fbbb9dbfd65c9691e8851dd
EluneyMo <morales.eluney.epet20@gmail.com>
2022-11-07 11:21:04 -0300

    Agrego mas contenedores a contenedor

--- js/Js_Productos/script.js
+++ js/Js_Productos/script.js
@@ -7,3 +7,13 @@ Suculentas.forEach((content) =>{
     <p>${content.precio}</p>`;
     contDeSuculentas.appendChild(div);
 })
+
+const contDeAcuaticas = document.getElementById('Acuaticas');
+
+Acuaticas.forEach((content) =>{
+    const div = document.createElement('div');
+    div.classList.add('content');
+    div.innerHTML = `<h1>${content.nombre}</h1>
+    <p>${content.precio}</p>`;
+    contDeAcuaticas.appendChild(div);
+})
