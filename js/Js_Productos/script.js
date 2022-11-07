b862137d4ffa651b25224605854b5c8f4614b973
EluneyMo <morales.eluney.epet20@gmail.com>
2022-11-07 11:04:54 -0300

    Modifico identificadores de js

--- js/Js_Productos/script.js
+++ js/Js_Productos/script.js
@@ -1,9 +1,9 @@
-const contDeProductos = document.getElementById('contProductos');
+const contDeSuculentas = document.getElementById('Suculentas');
 
-Plantas.forEach((content) =>{
+Suculentas.forEach((content) =>{
     const div = document.createElement('div');
     div.classList.add('content');
     div.innerHTML = `<h1>${content.nombre}</h1>
     <p>${content.precio}</p>`;
-    contDeProductos.appendChild(div);
+    contDeSuculentas.appendChild(div);
 })
