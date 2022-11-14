f5d6b7c8d07e7fe998b0a943940c2a52a821d2bd
EluneyMo <morales.eluney.epet20@gmail.com>
2022-11-14 14:05:46 -0300

    Eliminando errores

--- js/script.js
+++ js/script.js
@@ -33,28 +33,7 @@ function Mostrarboton(){
 document.getElementById ('hovers').style.visibility='visible';
 document.getElementById ('hovers2').style.visibility='visible';
 }
-const girasol = document.getElementById('girasol');
-const correo = document.getElementById('correo');
-const telefono= document.getElementById('tel');
-const asunto= document.getElementById('asun');
-const body = document.querySelector('body');
-const anchoMitad = window.innerWidth / 2;
-const altoMitad = window.innerHeight / 2;
-let seguirPunteroMouse = true;
 
-body.addEventListener('mousemove', (m) => {
-  if (seguirPunteroMouse) {
-    if (m.clientX < anchoMitad && m.clientY < altoMitad) {
-      girasol.src = "../imagenes/1-PhotoRoom.png";
-    }else if (m.clientX < anchoMitad && m.clientY > altoMitad) {
-      girasol.src = "../imagenes/2-removebg-preview.png";
-    }else if (m.clientX > anchoMitad && m.clientY < altoMitad) {
-      girasol.src = "../imagenes/1-removebg-preview.png";
-    }else{
-      girasol.src="../imagenes/2-PhotoRoom.png";
-    }
-  }
-})
 function mover(){
   correo=document.getElementById("correo").value;
   tel=document.getElementById("tel").value;
