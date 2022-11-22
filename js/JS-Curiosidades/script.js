aef9d9a90adc64bd360328e8e17b77d400eae3f7
46330019mercado <mercadonatali.epet20@gmail.com>
2022-11-22 13:08:41 -0300

    Creando un if para el boton

--- js/JS-Curiosidades/script.js
+++ js/JS-Curiosidades/script.js
@@ -3,4 +3,10 @@ function mouseover(){
      const name=document.forms['suForm']['name'].value;
      const contra=document.forms['suForm']['contra'].value;
      let validate=/[x-zA-Z0-9]{5}/;
+
+     if ((name=="" || !contra.match(validate))&& x==0){
+        buttonMoverLeft();
+        x=1;
+        return false;      
+     }
 }
