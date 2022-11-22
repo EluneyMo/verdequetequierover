262c9334197f1fc5d8eb61e693fe51ac372acd30
46330019mercado <mercadonatali.epet20@gmail.com>
2022-11-22 13:05:24 -0300

    Agregando variable let

--- js/JS-Curiosidades/script.js
+++ js/JS-Curiosidades/script.js
@@ -1,5 +1,6 @@
 var x=0;
- function mouseover(){
+function mouseover(){
      const name=document.forms['suForm']['name'].value;
      const contra=document.forms['suForm']['contra'].value;
- }
+     let validate=/[x-zA-Z0-9]{5}/;
+}
