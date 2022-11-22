9b8ac19c921fad66aa3f74a628189d6a106baff2
46330019mercado <mercadonatali.epet20@gmail.com>
2022-11-22 14:15:25 -0300

    Agregando target

--- js/JS-Curiosidades/script.js
+++ js/JS-Curiosidades/script.js
@@ -75,6 +75,8 @@ $(function(){
       $('x[href*=#]:not([href=#])').click(function(){
          if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')&& location.hostname==this.hostname){
               var target=$(this.hash);
+              target=target.length ?target : $('[name='+this.hash.slice(1)+']');
+              
          }
       })
 })
